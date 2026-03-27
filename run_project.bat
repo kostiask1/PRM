6a7aa04964e8e42e9a7eee62aa06a724c1984a08
Kostiantyn K <kostiasha99@gmail.com>
2026-03-27 14:02:48 +0200

    FIX fixed start project

--- run_project.bat
+++ run_project.bat
@@ -1,18 +1,7 @@
 @echo off
 cd /d %~dp0
 
-set PORT=3000
-
-:find_port
-netstat -ano | findstr /r /c:":%PORT% " | findstr LISTENING >nul
-if %errorlevel% equ 0 (
-    echo Port %PORT% is busy, trying %PORT%+1...
-    set /a PORT+=1
-    goto find_port
-)
-
-echo Starting Node server on port %PORT%...
-start cmd /k "set PORT=%PORT%&& npm start"
+start cmd /k "npm start"
 
 echo Waiting for server to start...
 timeout /t 3 > nul
