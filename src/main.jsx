d8daae8ce27b0081f0863403ade0bf9829950d9c
Kostiantyn K <kostiasha99@gmail.com>
2026-03-30 22:06:20 +0300

    FIX sw name

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('../sw.js')
+    navigator.serviceWorker.register('../service-worker.js')
       .then(registration => console.log('SW registered: ', registration))
       .catch(registrationError => console.log('SW registration failed: ', registrationError));
   });
