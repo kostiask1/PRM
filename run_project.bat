fe16827d19e7e632ec6c519e20ab8d7e5f5767ac
Kostiantyn K <kostiasha99@gmail.com>
2026-04-01 16:55:37 +0300

    FTR first launch of project

--- run_project.bat
+++ run_project.bat
@@ -1,6 +1,17 @@
 @echo off
 cd /d %~dp0
 
+REM Перевірка наявності папки node_modules
+IF NOT EXIST node_modules (
+    echo Папку node_modules не знайдено. Запускаємо npm install...
+    call npm i
+    IF %ERRORLEVEL% NEQ 0 (
+        echo npm install завершився з помилкою. Вихід.
+        pause
+        exit /b 1
+    )
+)
+
 start cmd /k "npm start"
 
 echo Waiting for server to start...
