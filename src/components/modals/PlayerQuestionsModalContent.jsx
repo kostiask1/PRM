ccf35f8dfc0c8a33cb9ae3786b6c5d3244e4ada0
Kostiantyn K <kostiasha99@gmail.com>
2026-05-30 13:13:59 +0300

    FIX tip text

--- src/components/modals/PlayerQuestionsModalContent.jsx
+++ src/components/modals/PlayerQuestionsModalContent.jsx
@@ -173,7 +173,7 @@ export default function PlayerQuestionsModalContent() {
 				</div>
 				<div className="PlayerQuestionsModalContent__hint">
 					{lang.t(
-						"Instead of rolling 1d{count}, you can roll (1d6 - 1) + 1d100 + 1d10.",
+						"Alternative: 1d6 - 1 gives the hundreds (0-5), then 1d100 gives the number in that hundred.",
 						{ count: QUESTIONS_COUNT },
 					)}
 				</div>
